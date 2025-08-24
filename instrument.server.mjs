1cd88ab513d53672aeb1f470c504ca6ae4e0aead
mohamedabdelstar06 <mohamedabdelstar06@gmail.com>
2025-08-24 16:50:43 +0300

    Enterprise Error tracking & Monitoring

---
+++ instrument.server.mjs
@@ -0,0 +1,14 @@
+import * as Sentry from "@sentry/react-router";
+import { nodeProfilingIntegration } from "@sentry/profiling-node";
+
+Sentry.init({
+  dsn: "https://86950cadaa7e94225657fc3fcd3025fd@o4509887649087488.ingest.us.sentry.io/4509887651971072",
+
+  // Adds request headers and IP for users, for more info visit:
+  // https://docs.sentry.io/platforms/javascript/guides/react-router/configuration/options/#sendDefaultPii
+  sendDefaultPii: true,
+
+  integrations: [nodeProfilingIntegration()],
+  tracesSampleRate: 1.0,
+  profilesSampleRate: 1.0,
+});
--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,23 @@ import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import {sentryReactRouter, type SentryReactRouterBuildOptions} from "@sentry/react-router"
 
-export default defineConfig({
-  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+const sentryConfig: SentryReactRouterBuildOptions = {
+  org: "abdelstar",
+  project: "travel-agency",
+  // An auth token is required for uploading source maps;
+  // store it in an environment variable to keep it secure.
+  authToken:"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"
+  // ...
+};
+
+export default defineConfig(config => {
+  return {
+  plugins: [tailwindcss(), tsconfigPaths(), reactRouter(),sentryReactRouter(sentryConfig, config)],
   ssr :{
     noExternal:[/@syncfusion/]
   }
+  };
 });
- 
+
