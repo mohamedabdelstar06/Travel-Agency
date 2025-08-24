be5faef05105bae55740c42c708d17b7e6f4d2e8
mohamedabdelstar06 <mohamedabdelstar06@gmail.com>
2025-08-24 14:14:51 +0300

    Add Login With Google Auth

--- app/routes.ts
+++ app/routes.ts
@@ -4,6 +4,7 @@ import { type RouteConfig, route ,layout } from "@react-router/dev/routes";
 
 
 export default [
+  route('sign-in', 'routes/root/sign-in.tsx'),
   layout( "routes/Admin/admin-layout.tsx",
     [
         route( 'dashboard', 'routes/Admin/dashboard.tsx'),
--- app/routes/Admin/admin-layout.tsx
+++ app/routes/Admin/admin-layout.tsx
@@ -1,8 +1,26 @@
 import { MobileSidebar, NavItems } from "Components";
-import { Outlet } from "react-router";
+import { Outlet, redirect } from "react-router";
 import { SidebarComponent } from "@syncfusion/ej2-react-navigations";
 import Dashboard from "./dashboard";
+import { account } from "~/appwrite/client";
+import { getExistingUser, storeUserData } from "~/appwrite/auth";
 
+export async function clientLoader(){
+  try{
+    const user = await account.get();
+    if(!user.$id) return redirect ('/sign-in');
+
+    const existingUser = await getExistingUser(user.$id);
+    if(existingUser?.status ==='user'){
+      return redirect('/');
+    }
+    return existingUser?.$id ? existingUser :await storeUserData()
+  }
+  catch(e){
+  console.log('Error in client loader', e);
+}
+return redirect('/sign-in');
+}
 const AdminLayout = () => {
   return (
     <div className="admin-layout flex">
